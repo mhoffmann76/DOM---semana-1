102fcb7ad2095b3653958131ad88708d3f1ae2db
Marcelo Hoffmann <52861216+mhoffmann76@users.noreply.github.com>
2023-01-24 08:22:57 -0300

    versao 2

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const precos = document.getElementsByClassName("produto_preco");
 var soma = 0;
 
 // Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
-for (let i = 0; i < precos.length; i++){
-   soma += parseFloat(precos[i].innerHTML);
-  
+for (let i = 0; i < precos.length; i++) {
+  soma += parseFloat(precos[i].innerHTML);
 }
-
 
 // Escrever no conteúdo da página o valor da soma
 
